3f961728d6d2d082cd714680ae48282980b33f3c
Tuan Ha <hatuan@email.cz>
2026-03-31 23:28:35 +0200

    Render general data according to inputs

--- src/components/CVSection.jsx
+++ src/components/CVSection.jsx
@@ -1,9 +1,15 @@
+import { format } from "date-fns";
+
+
 function GeneralInfo({data}) {
   return (
     <section>
       <h2>General Information</h2>
       <div>
-        <p>Name: {data.firstName + " " + data.lastName}</p>
+        <p><span>Name:</span> {data.firstName + " " + data.lastName}</p>
+        <p><span>Date of Birth:</span> {data.birthday ? format(data.birthday, "yyyy-MM-dd") : ""}</p>
+        <p><span>Email:</span> {data.email}</p>
+        <p><span>Phone Number:</span> {data.phone}</p>
       </div>
     </section>
   )
