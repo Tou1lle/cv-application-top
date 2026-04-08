b0dad10a2cceede8018a00b25ac63adf6a601a48
Tuan Ha <hatuan@email.cz>
2026-04-09 00:17:54 +0200

    Style inputs containers

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -7,7 +7,7 @@ export default function InputSection({type, data, onChange, onAdd}) {
     return (
       <section>
         <h2>General Information</h2>
-          <section>
+          <section className="general inputs-container">
             <Input 
               type="text" 
               id="firstName" 
@@ -52,27 +52,29 @@ export default function InputSection({type, data, onChange, onAdd}) {
     return (
       <section>
         <h2>Education</h2>
-        {data.map(school => (
-          <div key={school.id}>
-            <h3>{school.level.charAt(0).toUpperCase() + school.level.slice(1)}</h3>
-            <Input 
-              type="text" 
-              id="name" 
-              htmlID={`name-${school.level}`} 
-              label={school.level.charAt(0).toUpperCase() + school.level.slice(1) + " Name"} 
-              value={school.name} 
-              onChange={onChange} 
-              level={school.level}/>
-            <Input 
-              type="text" 
-              id="specialization" 
-              htmlID={`specialization-${school.level}`} 
-              label="Specialization" 
-              value={school.specialization} 
-              onChange={onChange} 
-              level={school.level}/>
-          </div>
-        ))}
+        <section className="education inputs-container">
+          {data.map(school => (
+            <div key={school.id}>
+              <h3>{school.level.charAt(0).toUpperCase() + school.level.slice(1)}</h3>
+              <Input
+                type="text"
+                id="name"
+                htmlID={`name-${school.level}`}
+                label={school.level.charAt(0).toUpperCase() + school.level.slice(1) + " Name"}
+                value={school.name}
+                onChange={onChange}
+                level={school.level}/>
+              <Input
+                type="text"
+                id="specialization"
+                htmlID={`specialization-${school.level}`}
+                label="Specialization"
+                value={school.specialization}
+                onChange={onChange}
+                level={school.level}/>
+            </div>
+          ))}
+        </section>
       </section>
     )
   }
@@ -81,45 +83,47 @@ export default function InputSection({type, data, onChange, onAdd}) {
     return (
       <section>
         <h2>Work Experience</h2>
-        {data.map((work, id) => (
-          <div key={work.id}>
-            <h3>Practical Experience {id + 1}</h3>
-            <Input
-              workID={work.id}
-              type="text"
-              id="name"
-              htmlID={"work-name" + work.id}
-              label="Company Name"
-              value={work.name}
-              onChange={onChange}/>
-            <Input 
-              workID={work.id}
-              type="text"
-              id="location"
-              htmlID={"work-location" + work.id}
-              label="Work Location"
-              value={work.location}
-              onChange={onChange}
-            />
-            <textarea name="work-description" id={"description" + work.id} value={work.description} onChange={e => onChange(e.target.value, "description", null, work.id)}></textarea>
-            <Input
-              workID={work.id}
-              type="date" 
-              id="startDate"
-              htmlID={"work-start-date" + work.id} 
-              label="Starting Date" 
-              value={work.startDate && format(work.startDate, "yyyy-MM-dd")} 
-              onChange={onChange}/>
-            <Input
-              workID={work.id}
-              type="date" 
-              id="endDate" 
-              htmlID={"work-end-date" + work.id}
-              label="Ending date" 
-              value={work.endDate && format(work.endDate, "yyyy-MM-dd")} 
-              onChange={onChange}/>
-          </div>
-        ))}
+        <section className="work inputs-container">
+          {data.map((work, id) => (
+            <div key={work.id}>
+              <h3>Practical Experience {id + 1}</h3>
+              <Input
+                workID={work.id}
+                type="text"
+                id="name"
+                htmlID={"work-name" + work.id}
+                label="Company Name"
+                value={work.name}
+                onChange={onChange}/>
+              <Input
+                workID={work.id}
+                type="text"
+                id="location"
+                htmlID={"work-location" + work.id}
+                label="Work Location"
+                value={work.location}
+                onChange={onChange}
+              />
+              <textarea name="work-description" id={"description" + work.id} value={work.description} onChange={e => onChange(e.target.value, "description", null, work.id)}></textarea>
+              <Input
+                workID={work.id}
+                type="date"
+                id="startDate"
+                htmlID={"work-start-date" + work.id}
+                label="Starting Date"
+                value={work.startDate && format(work.startDate, "yyyy-MM-dd")}
+                onChange={onChange}/>
+              <Input
+                workID={work.id}
+                type="date"
+                id="endDate"
+                htmlID={"work-end-date" + work.id}
+                label="Ending date"
+                value={work.endDate && format(work.endDate, "yyyy-MM-dd")}
+                onChange={onChange}/>
+            </div>
+          ))}
+        </section>
         <button onClick={onAdd}>Add Work Experience</button>
       </section>
     )
